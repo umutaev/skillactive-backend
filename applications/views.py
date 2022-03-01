eae0b192226448cebfd94f0f3dc1d0d99cd4415a
Max Umutaev <umutaev@Maxs-MacBook-Air.local>
2022-03-01 20:53:57 +0500

    Application management

--- applications/views.py
+++ applications/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from applications.models import ApplicationModel
 from applications.serializers import ApplicationSerializer
 from rest_framework.parsers import JSONParser
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from clubs.models import ClubModel
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
@@ -28,3 +28,28 @@ def applications(request, pk):
             serializer.save()
             return JsonResponse(serializer.data, status=201, json_dumps_params={"ensure_ascii": False})
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
+def application(request, pk_club, pk_application):
+    club = get_object_or_404(ClubModel, pk=pk_club)
+    application = get_object_or_404(ApplicationModel, pk=pk_application)
+    user = request.user
+    if not club.author == user and not user.is_staff:
+        raise PermissionDenied
+    if not application.club == club:
+        return HttpResponse(status=400)
+    if request.method == "GET":
+        serializer = ApplicationSerializer(application)
+        return JsonResponse(serializer.data, safe=True)
+    if request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ApplicationSerializer(application, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, json_dumps_params={"ensure_ascii": False})
+        return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        application.delete()
+        return HttpResponse(status=204)
--- clubs/urls.py
+++ clubs/urls.py
@@ -1,12 +1,13 @@
 from webbrowser import get
 from django.urls import path
 from clubs.views import club, post_club, get_clubs_user, search_club
-from applications.views import applications
+from applications.views import applications, application
 
 urlpatterns = [
     path("", post_club),
     path("my/", get_clubs_user),
     path("<int:pk>/", club),
     path("search/", search_club),
-    path("<int:pk>/applications", applications)
+    path("<int:pk>/applications", applications),
+    path("<int:pk_club>/applications/<int:pk_application>", application)
 ]

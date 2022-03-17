cd880aadec6d13224b20aaacb617bf0416b65f7d
Max Umutaev <umutaev@Maxs-MacBook-Air.local>
2022-03-17 22:17:17 +0500

    Added multipart form fileupload for experiment

--- app/fileupload/urls.py
+++ app/fileupload/urls.py
@@ -1,4 +1,7 @@
-from fileupload.views import FileUploadView
+from fileupload.views import FileUploadView, MultipartFileUploadView
 from django.urls import re_path
 
-urlpatterns = [re_path(r"upload/(?P<filename>[^/]+)$", FileUploadView.as_view())]
+urlpatterns = [
+    re_path(r"upload/(?P<filename>[^/]+)$", FileUploadView.as_view()),
+    re_path(r"multipart/(?P<filename>[^/]+)$", MultipartFileUploadView.as_view()),
+]
--- app/fileupload/views.py
+++ app/fileupload/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.parsers import FileUploadParser
+from rest_framework.parsers import FileUploadParser, MultiPartParser
 from django.core.files.storage import FileSystemStorage
 from uuid import uuid4
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 class FileUploadView(APIView):
@@ -11,6 +12,23 @@ class FileUploadView(APIView):
 
     def put(self, request, filename, format=None):
         file_obj = request.FILES["file"]
+        uuid_string = uuid4().hex
+        fss = FileSystemStorage()
+        file = fss.save(uuid_string + "_" + filename, file_obj)
+        file_url = fss.url(file)
+        return Response(status=201, data={"path": file_url})
+
+
+class MultipartFileUploadView(APIView):
+    parser_classes = [MultiPartParser]
+
+    def put(self, request, filename, format=None):
+        try:
+            file_obj = request.data["media"]
+        except MultiValueDictKeyError:
+            return Response(
+                status=400, data={"error": 'file should be in the "media" field'}
+            )
         uuid_string = uuid4().hex
         fss = FileSystemStorage()
         file = fss.save(uuid_string + "_" + filename, file_obj)

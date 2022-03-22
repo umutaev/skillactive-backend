4ed6ab28bfc229f9950cab803fafeec69babee93
umutaev <M8858110177@yandex.com>
2022-03-22 19:08:45 +0500

    application email template

--- app/applications/email.py
+++ app/applications/email.py
@@ -9,13 +9,13 @@ def send_application_email(application):
     mail_subject = f"Новая зявка на секцию {application.club.title} на skillactive.ru"
     message = f"{application.club.title}, {application.name}, {application.phone}, {application.text}"
     to_email = application.club.author.email
-    """html_page = render_to_string(
-        "mail.html",
+    html_page = render_to_string(
+        "application.html",
         {
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "uid": uid,
-            "token": token,
+            "club_name": application.club.title,
+            "name": application.name,
+            "phone": application.phone,
+            "text": application.text,
         },
-    )"""
-    send_mail(mail_subject, message, None, [to_email])  # , html_message=html_page)
+    )
+    send_mail(mail_subject, message, None, [to_email], html_message=html_page)
